c5e09ee5d98003490be123821245acf5df3a6e9c
Jahnava <jahnavalmt@gmail.com>
2019-12-13 12:46:01 -0500

    added css animation to the svg to create color changing pupils on th eyes

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var server = express();
 var port = process.env.PORT || 8080;
-server.use(express.static(__dirname + '/public/html'));
+server.use(express.static(__dirname + '/public'));
 server.get('/', function(req, res){
-  res.sendFile('index.html', {root:__dirname + '/public/html'});
+  res.sendFile('index.html', {root: __dirname + '/public/html'});
 });
 
 server.listen(port, function(){
-  console.log('Listening on this port riiighhht here ...', port);
+  console.log('Now listening on port ...', port);
 });
